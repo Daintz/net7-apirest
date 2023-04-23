a0038d5dcd24bc7f37b5a71a501214e7372bd4c5
D4intzz <81586667+Daintz@users.noreply.github.com>
2023-04-23 18:22:45 -0500

    feat: :construction: Add DataAnnotations
    Add DataAnnotations on villa model

--- Models/DTO/VillaDTO.cs
+++ Models/DTO/VillaDTO.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace ApiRest.Models.DTO
     public class VillaDTO
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
         public string Name { get; set; }
 
     }
