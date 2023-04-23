2604e30fc1698e8afa08f372ae364387a1b7fa55
D4intzz <81586667+Daintz@users.noreply.github.com>
2023-04-22 19:43:07 -0500

    feat: :construction: Add Controllers
    Add Villa Controller

---
+++ Controllers/VillaController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ApiRest.Models.DTO;
+using ApiRest.Data;
+
+namespace ApiRest.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VillaController : ControllerBase {
+    }
+}
