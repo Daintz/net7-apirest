702f91948360e8bf440e6798b091b90173250cc9
D4intzz <81586667+Daintz@users.noreply.github.com>
2023-04-22 20:02:35 -0500

    feat: :construction: Add GetVilla
    Add getVilla on Villas controller

--- Controllers/VillaController.cs
+++ Controllers/VillaController.cs
@@ -14,8 +14,29 @@ namespace ApiRest.Controllers
     {
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public ActionResult<IEnumerable<VillaDTO>> GetVillas() {
+        public ActionResult<IEnumerable<VillaDTO>> getVillas() {
             return Ok(VillaStore.villaList);
         }
+
+        [HttpGet("id:int")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<VillaDTO> getVilla(int id) {
+            if (id == 0) {
+                return BadRequest();
+            }
+
+            var villa = VillaStore.villaList.FirstOrDefault(v => v.Id == id);
+
+            if (villa == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(villa);
+        }
+
+        
     }
 }
