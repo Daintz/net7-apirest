a3ca88981a34ee52edb2931b60f5120e0d43517e
D4intzz <81586667+Daintz@users.noreply.github.com>
2023-05-01 09:11:53 -0500

    refactor: :construction: Change UpdateDTO
    Change UpdateDTO

--- Models/DTO/VillaUpdateDTO.cs
+++ Models/DTO/VillaUpdateDTO.cs
@@ -9,6 +9,8 @@ namespace ApiRest.Models.DTO
     public class VillaUpdateDTO
     {
         [Required]
+        public int Id { get; set; }
+        [Required]
         [MaxLength(30)]
         public string Name { get; set; }
     }
